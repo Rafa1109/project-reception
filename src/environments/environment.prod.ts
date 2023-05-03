71e1add3a3274ee078ebb0a6a5a418d413af9187
Neto Andrade <njaneto571@gmail.com>
2023-05-02 23:35:19 -0300

    new ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api: 'http://34.70.218.212:8080/api'
+  api: 'http://34.29.168.109:8080/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  api: 'http://34.70.218.212:8080/api'
+  api: 'http://34.29.168.109:8080/api'
 };
 
 /*
