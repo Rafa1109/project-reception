64ab717be3cbb9b1e07c119b2b88b6aa2dbb54df
Rafael Machado <rafael.machado.c11@gmail.com>
2023-05-16 00:37:07 -0300

    add Validators.requered

--- src/app/components/base-form/base-form.component.ts
+++ src/app/components/base-form/base-form.component.ts
@@ -1,37 +1,51 @@
 import { AfterViewInit, Component, ElementRef, ViewChildren } from "@angular/core";
-import { FormControlName, UntypedFormGroup } from "@angular/forms";
+import { AbstractControl, FormControlName, UntypedFormGroup, Validators } from "@angular/forms";
 import { fromEvent, merge, Observable } from "rxjs";
 import { FormValidatorHelper, ValidationMessages } from "src/app/core/helpers/form-validation";
 
 @Component({
-    template: ''
+  template: ''
 })
 export abstract class BaseForm implements AfterViewInit {
-    @ViewChildren(FormControlName, { read: ElementRef })
+  @ViewChildren(FormControlName, { read: ElementRef })
 
-    formInputElements: ElementRef[] = [];
-    form!: UntypedFormGroup;
-    validationMessages: ValidationMessages | undefined;
-    formValidator: FormValidatorHelper | undefined;
-    displayMessage: any = {};
-    constructor() {}
+  formInputElements: ElementRef[] = [];
+  form!: UntypedFormGroup;
+  validationMessages: ValidationMessages | undefined;
+  formValidator: FormValidatorHelper | undefined;
+  displayMessage: any = {};
+  constructor() { }
 
-    ngAfterViewInit(): void {
-        let controlBlurs: Observable<any>[] = this.formInputElements.map(
-          (formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur')
-        );
-    
-        merge(...controlBlurs).subscribe(() => {
-          this.displayMessage = this.formValidator?.processar(this.form);
-        });
-      }
-    
-      checkValidity(key: string) {
-        this.form.controls[key].markAsDirty();
-        this.displayMessage = this.formValidator?.processar(this.form);
-      }
-    
-      clearMessage = () => {
-        this.displayMessage = {};
-      };
+  ngAfterViewInit(): void {
+    let controlBlurs: Observable<any>[] = this.formInputElements.map(
+      (formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur')
+    );
+
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.formValidator?.processar(this.form);
+    });
+  }
+
+  checkValidity(key: string) {
+    this.form.controls[key].markAsDirty();
+    this.displayMessage = this.formValidator?.processar(this.form);
+  }
+
+  clearMessage = () => {
+    this.displayMessage = {};
+  };
+
+  updateValidators(keys: string[]): void {
+    Object.keys(this.form.controls).forEach(key => {
+      const control = this.form.get(key);
+
+      if (keys.includes(key))
+        control?.setValidators([Validators.required]);
+      else
+        control?.clearValidators();
+
+      control?.updateValueAndValidity();
+
+    })
+  }
 }
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  api: 'http://localhost:8080/api'
+  // api: 'http://localhost:8080/api'
+  api: "http://recepcao-adosasco.com.br:8080/api"
 };
